996b95d9fac7460537d5c2895c66fabd1381c02e
Harry <ddembrs@gmail.com>
2023-03-17 13:25:53 +0800

    merge

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,8 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-<<<<<<< HEAD
 import fs from "fs"; // https use (打包請關閉)
 import path from "path"; // https use (打包請關閉)
-=======
-import fs from "fs"; // https use
-import path from "path"; // https use
->>>>>>> master
 
 export default defineConfig({
   plugins: [vue()],
@@ -22,19 +17,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 8088,
-<<<<<<< HEAD
-    https: { // https use (打包請關閉)
-      key: fs.readFileSync(path.resolve(__dirname, "localhost.key")),
-      cert: fs.readFileSync(path.resolve(__dirname, "localhost.crt"))
-    },
-    proxy: {
-      "/api": {
-        target: "http://bgec.dev-relithe.com" // 泰爾開發站
-        // changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ""),
-      }
-    }
-=======
     https: { // https use
       key: fs.readFileSync(path.resolve(__dirname, "localhost.key")),
       cert: fs.readFileSync(path.resolve(__dirname, "localhost.crt"))
@@ -46,7 +28,6 @@ export default defineConfig({
     //     // rewrite: (path) => path.replace(/^\/api/, ""),
     //   }
     // }
->>>>>>> master
   },
   css: {
     preprocessorOptions: {
