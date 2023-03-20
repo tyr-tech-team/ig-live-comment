7237a3a52a90d359821ac169f883dc795eea04ed
Harry <ddembrs@gmail.com>
2023-03-20 13:56:38 +0800

    localStorage

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import * as $fbFn from "@/plugin/fbSDK";
+import * as $storage from "@/plugin/storage";
 import antd from "@/plugin/antd";
 import moment from "moment";
 import App from "./App.vue";
 
 const app = createApp(App);
 app.config.globalProperties.$fb = $fbFn;
+app.config.globalProperties.$storage = $storage;
 app.config.globalProperties.$moment= moment;
 app.use(createPinia());
 app.use(antd);
--- src/plugin/storage/index.js
+++ src/plugin/storage/index.js
@@ -1,8 +1,44 @@
-// import Vue from "vue";
-import * as storage from "./fn.js";
+// key對應表
+export const keys = {
+  theme: "THEME", // @param object，主題
+};
 
-// Vue.prototype.$storage = storage;
+// 設定項目
+export const Set = function (key, valueObj) {
+  try {
+    const _data = JSON.stringify(valueObj);
+    localStorage.setItem(key, _data);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
 
-// export default ({ app }) => {
-//   app.$storage = storage;
-// };
+// 提取項目
+export const Get = function (key = "") {
+  try {
+    const _data = localStorage.getItem(key);
+    return JSON.parse(_data);
+  } catch (error) {
+    return undefined;
+  }
+};
+
+// 清除項目
+export const Remove = function (key) {
+  try {
+    localStorage.removeItem(key);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+// 清除項目
+export const RemoveAll = function () {
+  try {
+    localStorage.clear(); // 全清
+  } catch (error) {
+    return false;
+  }
+};
